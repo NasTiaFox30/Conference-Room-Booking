23bf4050354dd2539b48fa2ae0788f1dfd97a560
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-09 17:41:58 +0100

    Created new Model : User.cs

---
+++ ConferenceRoomBookingSystem/Models/User.cs
@@ -0,0 +1,16 @@
+﻿namespace ConferenceRoomBookingSystem.Models
+{
+    public class User
+    {
+        public int UserId { get; set; }
+        public string Username { get; set; }
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Department { get; set; }
+        public string PasswordHash { get; set; }
+        public bool IsAdmin { get; set; }
+        public bool IsActive { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
+    }
+}
