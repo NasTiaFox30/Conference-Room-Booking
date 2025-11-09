14d42a933099274185b4a7d0f7e0e06d8e59b24d
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-09 18:05:46 +0100

    Add AssemblyInfo.cs in Properties ith all information

---
+++ ConferenceRoomBookingSystem/Properties/AssemblyInfo.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General information about an assembly is controlled through the following attributes.
+[assembly: AssemblyTitle("Conference Room Booking System")]
+[assembly: AssemblyDescription("DEMO Web system for booking conference rooms")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Conference Room Booking System")]
+[assembly: AssemblyCopyright("Copyright © Anastasiia Bzova 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components. If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("46a2a2dd-012a-41b0-abe4-49b096099e63")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
