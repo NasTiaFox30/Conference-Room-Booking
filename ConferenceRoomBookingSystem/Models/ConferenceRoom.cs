1bea334b3b33cd0e90346d541bf605f9762101f1
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-09 17:48:25 +0100

    Created model - ConferenceRoom

---
+++ ConferenceRoomBookingSystem/Models/ConferenceRoom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ConferenceRoomBookingSystem.Models
+{
+    public class ConferenceRoom
+    {
+        public int RoomId { get; set; }
+        public string RoomName { get; set; }
+        public int Capacity { get; set; }
+        public string Location { get; set; }
+        public string Description { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
