f7fd7f6bdc3825972833a6086763773b971cb57d
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-09 18:09:14 +0100

    Add Global.asax for logs dev errors

---
+++ ConferenceRoomBookingSystem/Global.asax.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Web;
+
+namespace ConferenceRoomBookingSystem
+{
+    public class Global : HttpApplication
+    {
+        protected void Application_Start(object sender, EventArgs e)
+        {
+            string appDataPath = Server.MapPath("~/App_Data");
+            AppDomain.CurrentDomain.SetData("DataDirectory", appDataPath);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            // Error handling logic:
+            Exception exc = Server.GetLastError();
+            // Here will log the error or notify developers
+        }
+    }
+}
