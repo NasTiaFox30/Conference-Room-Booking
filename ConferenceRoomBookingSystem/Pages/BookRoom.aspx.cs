fe0957b8fcfd6ba3592be96deb79cce436a2b089
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-11 21:28:32 +0100

    add private variables : roomId, room(ConferenceRoom object)

--- ConferenceRoomBookingSystem/Pages/BookRoom.aspx.cs
+++ ConferenceRoomBookingSystem/Pages/BookRoom.aspx.cs
@@ -8,6 +8,8 @@ namespace ConferenceRoomBookingSystem.Pages
 {
     public partial class BookRoom : Page
     {
+        private int roomId;
+        private ConferenceRoom room;
 
         protected void Page_Load(object sender, EventArgs e)
         {
