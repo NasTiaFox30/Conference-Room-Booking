e2d31082fe8f529081919ee1ffcc5d96a68d503f
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-11 20:40:00 +0100

    Empty BookRoom.aspx.cs

---
+++ ConferenceRoomBookingSystem/Pages/BookRoom.aspx.cs
@@ -0,0 +1,17 @@
+﻿using ConferenceRoomBookingSystem.Data;
+using ConferenceRoomBookingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Web.UI;
+
+namespace ConferenceRoomBookingSystem.Pages
+{
+    public partial class BookRoom : Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
