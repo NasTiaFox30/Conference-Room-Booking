23eaf3c98d7015bc826551293ba65afdcaaac331
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-11 15:02:41 +0100

    Created Data/BookingRepository.cs

---
+++ ConferenceRoomBookingSystem/Data/BookingRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using ConferenceRoomBookingSystem.Models;
+
+namespace ConferenceRoomBookingSystem.Data
+{
+    public class BookingRepository
+    {
+       
+    }
+}
