79d500888257fe0ecdf65c3af6119db0da661a37
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-11 19:10:20 +0100

    Created new empty class UsersRepository

---
+++ ConferenceRoomBookingSystem/Data/UsersRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using ConferenceRoomBookingSystem.Models;
+using BCrypt.Net;
+
+namespace ConferenceRoomBookingSystem.Data
+{
+    public class UsersRepository
+    {
+    }
+}
