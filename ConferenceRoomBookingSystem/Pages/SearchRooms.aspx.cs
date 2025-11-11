4cff7a70443fb9fd22b9483e6e6a6117fd71073e
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-11 14:38:28 +0100

    Created SearchRooms.aspx.cs

---
+++ ConferenceRoomBookingSystem/Pages/SearchRooms.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using ConferenceRoomBookingSystem.Data;
+using ConferenceRoomBookingSystem.Models;
+
+namespace ConferenceRoomBookingSystem.Pages
+{
+    public partial class SearchRooms : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+    }
+}
