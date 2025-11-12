2983d3a9cc9b6d7f4e76251c95559c29b55a86fc
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-12 19:40:05 +0100

    Created Site.Master.cs

--- ConferenceRoomBookingSystem/Site.Master.cs
+++ ConferenceRoomBookingSystem/Site.Master.cs
@@ -1,1 +1,11 @@
-﻿
+﻿using System;
+
+namespace ConferenceRoomBookingSystem
+{
+    public partial class SiteMaster : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+    }
+}
