e20f43e1508272256aacca571988232b7b802cb1
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-12 17:46:06 +0100

    Created empty Login.aspx.cs

---
+++ ConferenceRoomBookingSystem/Pages/Login.aspx.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Web.UI;
+using ConferenceRoomBookingSystem.Data;
+
+namespace ConferenceRoomBookingSystem.Pages
+{
+    public partial class Login : Page
+    {
+    }
+}
