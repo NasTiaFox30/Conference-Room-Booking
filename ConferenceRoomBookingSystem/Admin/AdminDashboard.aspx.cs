9d9ca5872abafaa48af6e8b136fb8a0c092d570b
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-22 19:24:03 +0100

    Added AdminDashboard.aspx.cs + Checking Admin role

---
+++ ConferenceRoomBookingSystem/Admin/AdminDashboard.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Web.UI;
+using ConferenceRoomBookingSystem.Data;
+
+namespace ConferenceRoomBookingSystem.Admin
+{
+    public partial class AdminDashboard : Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Check if user is admin
+                if (Session["IsAdmin"] == null || !(bool)Session["IsAdmin"])
+                {
+                    Response.Redirect("~/Pages/Default.aspx");
+                    return;
+                }
+
+                LoadStatistics();
+            }
+        }
+
+    }
+}
