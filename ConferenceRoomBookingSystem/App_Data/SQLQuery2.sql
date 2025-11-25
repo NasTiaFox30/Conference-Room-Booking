594716d232631545fcdfb62280dd62933db1c0da
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 20:05:04 +0100

    Update SQLQuery.sql: add test users query

--- ConferenceRoomBookingSystem/App_Data/SQLQuery2.sql
+++ ConferenceRoomBookingSystem/App_Data/SQLQuery2.sql
@@ -6,3 +6,9 @@ VALUES
 ('Sala Konferencyjna "Centralna"', 50, '3 piętro, pok. 301', 'Duża sala konferencyjna do prezentacji i wydarzeń firmowych', 1, 1, 1, 1),
 ('Sala szkoleniowa', 30, '2 piętro, pok. 210', 'Sala do prowadzenia szkoleń i wydarzeń edukacyjnych', 1, 1, 0, 1),
 ('Przestrzeń kreatywna', 15, '1 piętro, pok. 115', 'Nieformalna przestrzeń do burzy mózgów i sesji kreatywnych', 0, 1, 0, 1);
+
+-- test users for development and testing purposes
+INSERT INTO Users (Username, Email, FirstName, LastName, Department, PasswordHash, IsAdmin, IsActive)
+VALUES 
+('testuser', 'test@company.com', 'Test', 'User', 'IT', '$2a$11$examplehash', 0, 1),
+('adminuser', 'admin@company.com', 'Admin', 'User', 'Management', '$2a$11$adminhash', 1, 1);
