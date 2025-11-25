6331736b64a6c98ec67973418e1efe218e4496f8
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 23:40:18 +0100

    Creating Unit Tests: BookingRepositoryTests.cs

---
+++ ConferenceRoomBookingSystem.Tests/UnitTests/BookingRepositoryTests.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConferenceRoomBookingSystem.Data;
+using ConferenceRoomBookingSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConferenceRoomBookingSystem.Tests.UnitTests
+{
+    [TestClass]
+    public class BookingRepositoryTests
+    {
+    }
+}
