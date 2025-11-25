8899f8b111ae33edd607934dd7b0a3bada530629
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 21:50:06 +0100

    Deleted Cleanup_tables.sql

--- ConferenceRoomBookingSystem/App_Data/Cleanup_tables.sql
+++
@@ -1,6 +0,0 @@
-﻿-- 1. Clean parent table first to avoid foreign key constraint issues
-TRUNCATE TABLE [dbo].[Bookings];
-
--- 2. Clean child tables to remove dependencies
-TRUNCATE TABLE [dbo].[ConferenceRooms];
-TRUNCATE TABLE [dbo].[Users];
