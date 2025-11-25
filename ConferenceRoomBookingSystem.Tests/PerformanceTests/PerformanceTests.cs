00802af68e101ce778380829b80972b2826cd514
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 22:56:07 +0100

    Cretaed Performance test class - PerformanceTests.cs

---
+++ ConferenceRoomBookingSystem.Tests/PerformanceTests/PerformanceTests.cs
@@ -0,0 +1,14 @@
+﻿using ConferenceRoomBookingSystem.Data;
+using ConferenceRoomBookingSystem.Models;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace ConferenceRoomBookingSystem.Tests
+{
+    [TestClass]
+    public class PerformanceTests
+    {
+    }
+}
