f345c2b1a847247425ae77bda6c1777e8bf8eeee
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 18:41:18 +0100

    Created Unit MSTest Test Project (.NET Framework) + added reference to main project 'ConferenceRoomBookingSystem'

---
+++ ConferenceRoomBookingSystem.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ConferenceRoomBookingSystem.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ConferenceRoomBookingSystem.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("15f16493-3127-4158-9a8a-61edc062e9aa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
