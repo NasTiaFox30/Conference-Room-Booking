1afe235ae036ff615818a2ea4463c911620c30e4
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 23:11:16 +0100

    GetAllRooms_PerformanceTest_ShouldCompleteInReasonableTime() test - how fast is application is

--- ConferenceRoomBookingSystem.Tests/PerformanceTests/PerformanceTests.cs
+++ ConferenceRoomBookingSystem.Tests/PerformanceTests/PerformanceTests.cs
@@ -10,5 +10,21 @@ namespace ConferenceRoomBookingSystem.Tests
     [TestClass]
     public class PerformanceTests
     {
+        [TestMethod]
+        public void GetAllRooms_PerformanceTest_ShouldCompleteInReasonableTime()
+        {
+            // Arrange
+            var roomRepo = new ConferenceRoomRepository();
+            var stopwatch = new Stopwatch();
+
+            // Act
+            stopwatch.Start();
+            var rooms = roomRepo.GetAllRooms();
+            stopwatch.Stop();
+
+            // Assert
+            Assert.IsTrue(stopwatch.ElapsedMilliseconds < 1000,
+                $"GetAllRooms took {stopwatch.ElapsedMilliseconds}ms, expected less than 1000ms");
+        }
     }
 }
