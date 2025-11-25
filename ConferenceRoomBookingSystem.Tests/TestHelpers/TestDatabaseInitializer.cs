fce78a0fecdfa0cf499b132712fa7ea3df4a0e3f
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-25 22:48:04 +0100

    Create TestDatabaseInitializer.cs empty class

---
+++ ConferenceRoomBookingSystem.Tests/TestHelpers/TestDatabaseInitializer.cs
@@ -0,0 +1,10 @@
+﻿using ConferenceRoomBookingSystem.Data;
+using System;
+using System.Data.SqlClient;
+
+namespace ConferenceRoomBookingSystem.Tests.TestHelpers
+{
+    public static class TestDatabaseInitializer
+    {
+    }
+}
