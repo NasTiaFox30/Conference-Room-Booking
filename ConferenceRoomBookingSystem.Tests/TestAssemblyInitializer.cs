44841e0ff46c104c3230e02dfe3d3a5e1352f060
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-26 13:43:24 +0100

    Creted main test file : Initialization of the test environment... & Finish Tests...

---
+++ ConferenceRoomBookingSystem.Tests/TestAssemblyInitializer.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConferenceRoomBookingSystem.Tests
+{
+    [TestClass]
+    public class TestAssemblyInitializer
+    {
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext context)
+        {
+            System.Console.WriteLine(" == Initialization of the test environment...");
+            TestHelpers.TestDatabaseInitializer.InitializeTestDatabase();
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            System.Console.WriteLine(" == Finish Tests...");
+            TestHelpers.TestDatabaseInitializer.CleanupTestData();
+        }
+    }
+}
