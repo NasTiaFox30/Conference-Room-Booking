81894c10d72bd697881fc71d90453c33214874cf
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-26 13:36:48 +0100

    Created new Test file - UsersRepositoryTests.cs

---
+++ ConferenceRoomBookingSystem.Tests/UnitTests/UsersRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConferenceRoomBookingSystem.Data;
+using ConferenceRoomBookingSystem.Models;
+using System.Linq;
+
+namespace ConferenceRoomBookingSystem.Tests
+{
+    [TestClass]
+    public class UsersRepositoryTests
+    {
+    }
+}
