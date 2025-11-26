131d5cc834d2f72aeae4dad7bc1ee8f38a4c81d5
Anastasiia Bzova <bzovanastia@gmail.com>
2025-11-26 13:24:50 +0100

    Created empty tes file UsersRepositoryTests.cs

---
+++ ConferenceRoomBookingSystem.Tests/UnitTests/ConferenceRoomRepositoryTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConferenceRoomBookingSystem.Data;
+using ConferenceRoomBookingSystem.Models;
+using System.Collections.Generic;
+
+namespace ConferenceRoomBookingSystem.Tests
+{
+    [TestClass]
+    public class ConferenceRoomRepositoryTests
+    {
+    }
+}
